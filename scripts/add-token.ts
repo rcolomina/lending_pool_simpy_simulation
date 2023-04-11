5962deeddcbd94fa93d5a2826fa3eafc01f48e91
Ruben Colomina <rcolomina@gmail.com>
2023-04-11 22:37:22 +0100

    Add scripts to deploy contracts

--- scripts/add-token.ts
+++ scripts/add-token.ts
@@ -5,20 +5,49 @@ import { syncWriteFile } from "./utils.ts"
 
 async function main() {
 
-    // Get the contract owner
     const contractOwner = await ethers.getSigners();
-    console.log(`Deploying contract from: ${contractOwner[0].address}`);
+
+    console.log(`Deploying contract from contract owner: ${contractOwner[0].address}`);
+
+    ////
     
-    const ERC20Mock = await ethers.getContractFactory("ERC20Mock");
+    const ERC20Dai = await ethers.getContractFactory("ERC20Dai");
+    
+    console.log('Deploying ERC20Dai...');
+    const erc20Dai = await ERC20Dai.deploy();
+    await erc20Dai.deployed();
+    console.log(`Erc20Dai deployed to: ${erc20Dai.address}`)
 
-    // Deploy the contract
-    console.log('Deploying ERC20Mock...');
-    const erc20Mock = await ERC20Mock.deploy();
-    await erc20Mock.deployed();
-    console.log(`Erc20Mock deployed to: ${erc20Mock.address}`)
+    syncWriteFile('../deployed_token_address_dai.txt',
+                  erc20Dai.address);    
 
-    syncWriteFile('../deployed_token_address.txt', erc20Mock.address);    
+    ////
+    
+    const ERC20RubenCoin = await ethers.getContractFactory("ERC20RubenCoin");
+    
+    console.log('Deploying ERC20RubenCoin...');
+    const erc20RubenCoin = await ERC20RubenCoin.deploy();
+    await erc20RubenCoin.deployed();
+    console.log(`Erc20RubenCoin deployed to: ${erc20RubenCoin.address}`)
 
+    syncWriteFile('../deployed_token_address_ruben_coin.txt',
+                  erc20RubenCoin.address);    
+
+
+
+    ////
+    
+    const ERC20AlmanakCoin = await ethers.getContractFactory("ERC20AlmanakCoin");
+    
+    console.log('Deploying ERC20AlmanakCoin...');
+    const erc20AlmanakCoin = await ERC20AlmanakCoin.deploy();
+    await erc20AlmanakCoin.deployed();
+    console.log(`Erc20AlmanakCoin deployed to: ${erc20AlmanakCoin.address}`)
+
+    syncWriteFile('../deployed_token_address_almanak_coin.txt',
+                  erc20AlmanakCoin.address);    
+
+    
 }
 
 
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -7,7 +7,6 @@ async function main() {
     const LendingPool = await ethers.getContractFactory("LendingPool");
 
     console.log("Deploying LendingPool..");
-
     
     const lendingPool = await LendingPool.deploy();
 
